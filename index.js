3697186860df4f2661d9689f7801fa92a20f1010
Claudia <claudiaruizux@gmail.com>
2021-12-05 19:08:50 +1100

    completed tracker mode lab

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ function fetchAllCoins(){
     .then(json => json)
 }
 function onAddCoin(id, symbol, price){
-    // let addedCoin = document.getElementById(id);
-    // addedCoin.setAttribute("class", "addedCoin");
-
     return fetch('http://localhost:3000/myCoinResearch',{
         method:"POST",
         headers: {
@@ -17,7 +14,8 @@ function onAddCoin(id, symbol, price){
             symbol: symbol,
             price: price
         })
-    }) 
+
+    })   
 }
 function deleteMyCoin(id){
     return fetch(`http://localhost:3000/myCoinResearch/${id}`,{
@@ -29,38 +27,66 @@ function fetchMyCoins(){
     .then(reponse => reponse.json())
     .then(json => json)
 }
-document.addEventListener('DOMContentLoaded', () => {
-    fetchAllCoins().then(coins => {
-        Object.keys(coins).forEach(coin => {
-            let list = document.createElement('tr');
-            list.setAttribute("id", `${coins[coin].name}`)
 
-            let coinPrice = coins[coin].market_data.current_price.usd;
-            let priceConverted = (coinPrice).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-
-            list.innerHTML = `<td><img src="${coins[coin].image.small}" alt="${coins[coin].id}"></img>
-            <span><strong>${coins[coin].name}</strong>${coins[coin].symbol}</span></td>
-            <td class="price">$${priceConverted}</td>
-            <td><button type="button" onclick="onAddCoin('${coins[coin].name}', '${coins[coin].symbol}', '${coins[coin].market_data.current_price.usd}')" class="btn btn-primary">+</button></td>`;
-            document.querySelector('#all-coins').appendChild(list);
-        });
-    })
-    fetchMyCoins().then(coins => {
-        Object.keys(coins).forEach(coin => {
-            let list = document.createElement('tr');
-
-            let coinPrice = coins[coin].price;
-            // console.log(coins[coin].price);
-            //let priceConverted = (coinPrice).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-
-            list.innerHTML =
-            `
-            <td>
-            <span class="symbol">${coins[coin].symbol}</span>$${coinPrice}
-            <span onclick="deleteMyCoin(${coins[coin].id})" class="btn delete material-icons">delete_outline</span>
-            </td>
-            `;
-            document.querySelector('#my-coins').appendChild(list);
+function loadCoins(){
+    document.querySelector('#my-coins').innerHTML = "";
+    document.addEventListener('DOMContentLoaded', () => {
+        fetchAllCoins().then(coins => {
+            Object.keys(coins).forEach(coin => {
+                let list = document.createElement('tr');
+                list.setAttribute("id", `${coins[coin].name}`)
+    
+                let coinPrice = coins[coin].market_data.current_price.usd;
+                let priceConverted = new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 4 }).format(coinPrice);
+    
+                list.innerHTML = `
+                <td
+                onclick="onAddCoin(
+                    '${coins[coin].name}', 
+                    '${coins[coin].symbol}', 
+                    '${coins[coin].market_data.current_price.usd}')"
+                >
+                <img src="${coins[coin].image.small}" alt="${coins[coin].id}"></img>
+                <span><strong>${coins[coin].name}</strong>${coins[coin].symbol}</span>
+                </td>
+                <td 
+                    onclick="onAddCoin(
+                        '${coins[coin].name}', 
+                        '${coins[coin].symbol}', 
+                        '${coins[coin].market_data.current_price.usd}')"
+                    class="price">
+                    $${priceConverted}
+                </td>
+                <td><button type="button"
+                    onclick="onAddCoin(
+                        '${coins[coin].name}', 
+                        '${coins[coin].symbol}', 
+                        '${coins[coin].market_data.current_price.usd}')"
+                    class="btn btn-primary">+</button>
+                </td>`;
+                document.querySelector('#all-coins').appendChild(list);
+            });
         })
-    })
-});
+        fetchMyCoins().then(coins => {
+            Object.keys(coins).forEach(coin => {
+                let list = document.createElement('tr');
+                list.setAttribute("class", coins[coin].symbol)
+    
+                let coinPrice = coins[coin].price;
+                let priceConverted = new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 4 }).format(coinPrice);
+    
+                list.innerHTML =
+                `
+                <td onclick="deleteMyCoin(${coins[coin].id})">
+                <span class="symbol">${coins[coin].symbol}</span>$${priceConverted}
+                <span class="btn delete material-icons">delete_outline</span>
+                </td>
+                `;
+                document.querySelector('#my-coins').appendChild(list);
+            })
+        }) 
+        
+    });
+}
+
+setTimeout(loadCoins(), 3000);
